8e126262db4395dca2bece8b8a4f4c6d3840be0c
Aziret Alisher <aziretalisher@gmail.com>
2023-05-02 19:21:44 +0100

    Make App.js class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { Component } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
@@ -7,79 +7,88 @@ import Rank from "./components/Rank/Rank";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 
-function App() {
-  const particlesInit = useCallback(async (engine) => {
-    console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
-  }, []);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
 
-  const particlesLoaded = useCallback(async (container) => {
-    await console.log(container);
-  }, []);
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
 
-  return (
-    <div className="App">
-      <Particles
-        id="tsparticles"
-        className="particles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          fpsLimit: 120,
-          particles: {
-            color: {
-              value: "#ffffff",
-            },
-            links: {
-              color: "#ffffff",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              directions: "none",
-              enable: true,
-              outModes: {
-                default: "bounce",
+  onButtonSubmit = () => {
+    console.log("click");
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles
+          id="tsparticles"
+          className="particles"
+          init={async (engine) => {
+            await loadFull(engine);
+          }}
+          options={{
+            fpsLimit: 120,
+            particles: {
+              color: {
+                value: "#ffffff",
               },
-              random: false,
-              speed: 2,
-              straight: false,
-            },
-            number: {
-              density: {
+              links: {
+                color: "#ffffff",
+                distance: 150,
                 enable: true,
-                area: 800,
+                opacity: 0.5,
+                width: 1,
               },
-              value: 80,
+              collisions: {
+                enable: true,
+              },
+              move: {
+                directions: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 2,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 1, max: 5 },
+              },
             },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: { min: 1, max: 5 },
-            },
-          },
-          detectRetina: true,
-        }}
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+            detectRetina: true,
+          }}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
