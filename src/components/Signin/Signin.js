5ce48f081897524f9d2d3ee78ce40cd48e80ccf8
Aziret Alisher <aziretalisher@gmail.com>
2023-05-04 16:59:12 +0100

    Change form tag to div to prevent default behavior of the submit button

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -42,7 +42,7 @@ class Signin extends Component {
     return (
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw5 shadow-5 center">
         <main className="pa4 black-80">
-          <form className="measure center flex flex-column">
+          <div className="measure center flex flex-column">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f2 fw6 ph0 mh0">Sign In</legend>
               <div className="mt3">
@@ -88,7 +88,7 @@ class Signin extends Component {
                 Register
               </p>
             </div>
-          </form>
+          </div>
         </main>
       </article>
     );
